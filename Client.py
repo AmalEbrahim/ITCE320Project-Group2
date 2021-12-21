7c1b97bfb726a9d3f99b0229cce4a36d3bc96b21
Amal <amool.eb@gmail.com>
2021-12-21 09:22:29 +0300

    some edditing in establishing the connection

--- Client.py
+++ Client.py
@@ -1,4 +1,7 @@
 import socket
 print(25*"--"+"\n The Client Has Started \n")
+server_address = ("192.168.1.104", 52497)
+my_name = input("Enter client name: ")
 Csock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-Csock.connect(("192.168.1.104", 52497))
+Csock.connect(server_address)
+Csock.send(my_name.encode('ascii'))
