d2e0f4773795509d0982d2a84bd4d782c79d015f
noor <no0rhussaiin4@gmail.com>
2021-12-23 22:48:18 +0300

    update client

--- client/Client.py
+++ client/Client.py
@@ -8,12 +8,13 @@ server_address = ("192.168.43.201", 52499)
 
 Csock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 Csock.connect(server_address)
-my_name = input("Enter client name: ")
-Csock.send(my_name.encode('ascii'))
+my_name = input("Enter client name: ")   # ask the client to enter her name
+Csock.send(my_name.encode('ascii'))  # send the UserName to server
 arr_icao = input("\nEnter the airport code:").upper()
-Csock.send(arr_icao.encode('ascii'))
+Csock.send(arr_icao.encode('ascii'))  # send airport code to server
 
 while True:
+    # take the input option from the user
     print("\nSelect an Option:\n"
           "1. Display all arrived flights details.\n"
           "2. Display all delayed flights details.\n"
@@ -22,15 +23,16 @@ while True:
           "5. Quit.\n")
 
     number = input("Sending number: ")
-    Csock.sendall(number.encode('ascii'))
+    Csock.sendall(number.encode('ascii'))  # send the option number to server
     if int(number) == 1:
         print("All arrived flights : ")
         Directory = r'C:\Users\amool\PycharmProjects\ITCE320_Project\client\client_{}'.format(my_name)
+        # create directory if it is not found and save the information inside it
         if not os.path.exists(Directory):
             os.mkdir(Directory)
         file_path = Directory + '/G2.json'
 
-        with open(file_path, 'wb') as file:
+        with open(file_path, 'wb') as file:  # saving information in JSON file in client folder
             recvfile = Csock.recv(1048576)
             file.write(recvfile)
 
@@ -47,11 +49,12 @@ while True:
     elif int(number) == 2:
         print("All delayed flights : ")
         Directory = r'C:\Users\amool\PycharmProjects\ITCE320_Project\client\client_{}'.format(my_name)
+        # create directory if it is not found and save the information inside it
         if not os.path.exists(Directory):
             os.mkdir(Directory)
         file_path = Directory + '/G2.json'
 
-        with open(file_path, 'wb') as file:
+        with open(file_path, 'wb') as file:  # saving information in JSON file in client folder
             recvfile = Csock.recv(1048576)
             file.write(recvfile)
 
@@ -72,11 +75,12 @@ while True:
         Csock.sendall(cityName.encode('ascii'))
         print("All flights coming from {}: ".format(cityName))
         Directory = r'C:\Users\amool\PycharmProjects\ITCE320_Project\client\client_{}'.format(my_name)
+        # create directory if it is not found and save the information inside it
         if not os.path.exists(Directory):
             os.mkdir(Directory)
         file_path = Directory + '/G2.json'
 
-        with open(file_path, 'wb') as file:
+        with open(file_path, 'wb') as file:   # saving information in JSON file in client folder
             recvfile = Csock.recv(1048576)
             file.write(recvfile)
 
@@ -98,12 +102,12 @@ while True:
 
         print("Details of flight {}".format(flightNum))
         Directory = r'C:\Users\amool\PycharmProjects\ITCE320_Project\client\client_{}'.format(my_name)
-
+        # create directory if it is not found and save the information inside it
         if not os.path.exists(Directory):
             os.mkdir(Directory)
             file_path = Directory + '/G2.json'
 
-            with open(file_path, 'wb') as file:
+            with open(file_path, 'wb') as file:  # saving information in JSON file in client folder
                 recvfile = Csock.recv(1035)
                 file.write(recvfile)
 
